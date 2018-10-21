0ef17f312f985a9230b4e6e615bb2192c1e16c62
David Moore <mooredev@me.com>
2018-10-20 21:46:12 -0400

    Initial commit

---
+++ Hardware/remote_core/remote_core/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  remote_core
+//
+//  Created by David Moore on 10/20/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
