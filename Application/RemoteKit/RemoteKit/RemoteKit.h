ef71a6f928b6a158a3a396f885371b81e97a2df7
David Moore <david@mooredev.ca>
2018-10-22 22:50:28 -0400

    Initial commit

---
+++ Application/RemoteKit/RemoteKit/RemoteKit.h
@@ -0,0 +1,19 @@
+//
+//  RemoteKit.h
+//  RemoteKit
+//
+//  Created by David Moore on 10/22/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RemoteKit.
+FOUNDATION_EXPORT double RemoteKitVersionNumber;
+
+//! Project version string for RemoteKit.
+FOUNDATION_EXPORT const unsigned char RemoteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteKit/PublicHeader.h>
+
+
---
+++ Application/RemoteKit/RemoteKitTests/RemoteKitTests.swift
@@ -0,0 +1,25 @@
+//
+//  RemoteKitTests.swift
+//  RemoteKitTests
+//
+//  Created by David Moore on 10/22/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+import XCTest
+@testable import RemoteKit
+
+class RemoteKitTests: XCTestCase {
+
+    // MARK: - Lifecycle
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    // MARK: - Tests
+}
