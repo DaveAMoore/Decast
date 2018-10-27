1883e00bc062e5a4bb2a940781a8089fa7640b49
David Moore <mooredev@me.com>
2018-10-27 15:20:06 -0400

    Update scripts

--- Scripts/build-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd Hardware/aws-iot-device-sdk-cpp/
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ../.
-make aws-iot-sdk-cpp
--- Scripts/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd Hardware/remote_core/remote_core
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ../.
