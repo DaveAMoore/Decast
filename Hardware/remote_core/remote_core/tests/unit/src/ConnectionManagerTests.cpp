43045d8299523eede52c4bec117c3c365fc6a990
David Moore <mooredev@me.com>
2018-10-28 14:14:29 -0400

    Write first unit test for the ConnectionManagerTests suite

--- Hardware/remote_core/remote_core/tests/unit/src/ConnectionManagerTests.cpp
+++ Hardware/remote_core/remote_core/tests/unit/src/ConnectionManagerTests.cpp
@@ -6,9 +6,19 @@
 //  Copyright © 2018 David Moore. All rights reserved.
 //
 
+#include <memory>
 #include <gtest/gtest.h>
 #include "ConnectionManager.hpp"
 
-TEST(ConnectionManagerTests, Foo) {
+using namespace awsiotsdk;
+using namespace RemoteCore;
+
+TEST(ConnectionManagerTests, ResumeAndSuspendConnection) {
+    auto connectionManager = std::make_unique<ConnectionManager>("config/remote_core_config.json");
     
+    ResponseCode responseCode = connectionManager->resumeConnection();
+    ASSERT_EQ(responseCode, ResponseCode::MQTT_CONNACK_CONNECTION_ACCEPTED);
+    
+    responseCode = connectionManager->suspendConnection();
+    ASSERT_EQ(responseCode, ResponseCode::SUCCESS);
 }
