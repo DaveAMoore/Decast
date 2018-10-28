4dcecda35283243013189b34cf1e84a241c56e0e
David Moore <mooredev@me.com>
2018-10-27 21:07:40 -0400

    Create first test case

---
+++ Hardware/remote_core/remote_core/tests/unit/src/ConnectionManagerTests.cpp
@@ -0,0 +1,14 @@
+//
+//  ConnectionManagerTests.cpp
+//  remote_core
+//
+//  Created by David Moore on 10/27/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+#include "ConnectionManager.hpp"
+
+TEST(ConnectionManagerTests, Foo) {
+    
+}
