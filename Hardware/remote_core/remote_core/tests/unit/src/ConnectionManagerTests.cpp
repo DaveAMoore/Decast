7d720996d985adc58bb06103fbf0dc5d8e783090
David Moore <mooredev@me.com>
2018-10-29 00:14:09 -0400

    Start adding GMock

--- Hardware/remote_core/remote_core/tests/unit/src/ConnectionManagerTests.cpp
+++ Hardware/remote_core/remote_core/tests/unit/src/ConnectionManagerTests.cpp
@@ -13,12 +13,29 @@
 using namespace awsiotsdk;
 using namespace RemoteCore;
 
-TEST(ConnectionManagerTests, ResumeAndSuspendConnection) {
-    auto connectionManager = std::make_unique<ConnectionManager>("config/remote_core_config.json");
+#define DEFAULT_TOPIC_NAME "topic_1"
+
+// MARK: - Test Fixture
+
+class ConnectionManagerTests : public testing::Test {
+protected:
+    std::shared_ptr<ConnectionManager> connectionManager;
     
+    void SetUp() override {
+        connectionManager = std::make_shared<ConnectionManager>("config/remote_core_config.json");
+    }
+};
+
+// MARK: - Tests
+
+TEST_F(ConnectionManagerTests, ResumeAndSuspendConnection) {
     ResponseCode responseCode = connectionManager->resumeConnection();
     ASSERT_EQ(responseCode, ResponseCode::MQTT_CONNACK_CONNECTION_ACCEPTED);
     
     responseCode = connectionManager->suspendConnection();
     ASSERT_EQ(responseCode, ResponseCode::SUCCESS);
+}
+
+TEST_F(ConnectionManagerTests, SubscribeToTopic) {
+    /*connectionManager->subscribeToTopic(DEFAULT_TOPIC_NAME, <#MessageHandler messageHandler#>, <#CompletionHandler completionHandler#>);*/
 }
