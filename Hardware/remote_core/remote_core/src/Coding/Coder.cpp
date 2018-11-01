036cf401b70f60710c0dc5e6a2cb3895433916cb
David Moore <mooredev@me.com>
2018-11-01 09:46:41 -0400

    Add empty implementation file for 'Coder'

---
+++ Hardware/remote_core/remote_core/src/Coding/Coder.cpp
@@ -0,0 +1,12 @@
+//
+//  Coder.cpp
+//  remote_core
+//
+//  Created by David Moore on 10/31/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+#include "Coder.hpp"
+
+using namespace RemoteCore;
+
