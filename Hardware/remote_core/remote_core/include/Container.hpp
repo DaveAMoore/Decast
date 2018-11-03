12b16f9663cc8585cd328a500f1b071bffe41c22
David Moore <mooredev@me.com>
2018-11-03 01:33:36 -0400

    Add encodable interface

--- Hardware/remote_core/remote_core/include/Container.hpp
+++ Hardware/remote_core/remote_core/include/Container.hpp
@@ -10,17 +10,25 @@
 #define Container_hpp
 
 #include <iostream>
+#include <memory>
 
 namespace RemoteCore {
     class Container {
     public:
+        virtual ~Container() {};
+        
         virtual void encodeIntForKey(int value, std::string key) = 0;
         virtual void encodeBoolForKey(bool value, std::string key) = 0;
         virtual void encodeStringForKey(std::string value, std::string key) = 0;
         
-        virtual int decodeIntForKey(std::string key) = 0;
-        virtual bool decodeBoolForKey(std::string key) = 0;
-        virtual std::string decodeStringForKey(std::string key) = 0;
+        virtual std::unique_ptr<Container> requestEncodableContainer() = 0;
+        virtual void submitEncodableContainerForKey(std::unique_ptr<Container> encodableContainer,
+                                                    std::string key) = 0;
+        
+        virtual int intForKey(std::string key) = 0;
+        virtual bool boolForKey(std::string key) = 0;
+        virtual std::string stringForKey(std::string key) = 0;
+        virtual std::unique_ptr<Container> containerForKey(std::string key) = 0;
     };
 }
 
