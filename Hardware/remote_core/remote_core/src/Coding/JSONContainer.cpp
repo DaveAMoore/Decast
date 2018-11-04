17312f45140d985d1dbd6c79fd7229ad2973c8d4
David Moore <mooredev@me.com>
2018-11-04 00:11:22 -0400

    Add support for new data types

--- Hardware/remote_core/remote_core/src/Coding/JSONContainer.cpp
+++ Hardware/remote_core/remote_core/src/Coding/JSONContainer.cpp
@@ -27,6 +27,14 @@ void JSONContainer::setIntForKey(int value, std::string key) {
     internalContainer[key] = value;
 }
 
+void JSONContainer::setUnsignedIntForKey(unsigned int value, std::string key) {
+    internalContainer[key] = value;
+}
+
+void JSONContainer::setFloatForKey(double value, std::string key) {
+    internalContainer[key] = value;
+}
+
 void JSONContainer::setBoolForKey(bool value, std::string key) {
     internalContainer[key] = value;
 }
@@ -35,32 +43,68 @@ void JSONContainer::setStringForKey(std::string value, std::string key) {
     internalContainer[key] = value;
 }
 
-std::unique_ptr<Container> JSONContainer::requestEncodableContainer() {
+std::unique_ptr<Container> JSONContainer::createNestedContainer() {
     return std::make_unique<JSONContainer>();
 }
 
-void JSONContainer::submitEncodableContainerForKey(std::unique_ptr<Container> encodableContainer, std::string key) {
-    auto castEncodableContainer = std::unique_ptr<JSONContainer>(static_cast<JSONContainer *>(encodableContainer.release()));
-    internalContainer[key] = castEncodableContainer->internalContainer;
+void JSONContainer::setNestedContainerForKey(std::unique_ptr<Container> nestedContainer, std::string key) {
+    auto castNestedContainer = std::unique_ptr<JSONContainer>(static_cast<JSONContainer *>(nestedContainer.release()));
+    internalContainer[key] = castNestedContainer->internalContainer;
 }
 
 // MARK: - Decoding
 
 int JSONContainer::intForKey(std::string key) {
-    return internalContainer[key].get<int>();
+    auto value = internalContainer[key];
+    if (value.is_number()) {
+        return value.get<int>();
+    } else {
+        return 0;
+    }
+}
+
+unsigned int JSONContainer::unsignedIntForKey(std::string key) {
+    auto value = internalContainer[key];
+    if (value.is_number()) {
+        return value.get<unsigned int>();
+    } else {
+        return 0;
+    }
+}
+
+double JSONContainer::floatForKey(std::string key) {
+    auto value = internalContainer[key];
+    if (value.is_number()) {
+        return value.get<double>();
+    } else {
+        return 0.0;
+    }
 }
 
 bool JSONContainer::boolForKey(std::string key) {
-    return internalContainer[key].get<bool>();
+    auto value = internalContainer[key];
+    if (value.is_boolean()) {
+        return value.get<bool>();
+    } else {
+        return false;
+    }
 }
 
 std::string JSONContainer::stringForKey(std::string key) {
-    return internalContainer[key].get<std::string>();
+    auto value = internalContainer[key];
+    if (value.is_string()) {
+        return value.get<std::string>();
+    } else {
+        return NULL;
+    }
 }
 
 std::unique_ptr<Container> JSONContainer::containerForKey(std::string key) {
-    auto value = internalContainer[key].get<json>();
-    auto container = std::make_unique<JSONContainer>(value);
-    
-    return container;
+    auto value = internalContainer[key];
+    if (value.is_object()) {
+        auto container = std::make_unique<JSONContainer>(value.get<json>());
+        return container;
+    } else {
+        return nullptr;
+    }
 }
