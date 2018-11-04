d3cac221a23e9d9135982b05a26099d604bb77bf
David Moore <mooredev@me.com>
2018-11-04 00:14:32 -0400

    Finish JSONContainer Test Suite

--- Hardware/remote_core/remote_core/tests/unit/src/JSONContainerTests.cpp
+++ Hardware/remote_core/remote_core/tests/unit/src/JSONContainerTests.cpp
@@ -16,9 +16,27 @@ TEST(JSONContainerTests, EncodeDecodeInt) {
     JSONContainer container;
 
     const int value = INT_MAX;
-    container.encodeIntForKey(value, "EncodeDecodeInt");
+    container.setIntForKey(value, "EncodeDecodeInt");
     
     ASSERT_EQ(value, container.intForKey("EncodeDecodeInt"));
+}
+
+TEST(JSONContainerTests, EncodeDecodeUnsignedInt) {
+    JSONContainer container;
+    
+    const unsigned int value = UINT_MAX;
+    container.setUnsignedIntForKey(value, "EncodeDecodeUnsignedInt");
+    
+    ASSERT_EQ(container.unsignedIntForKey("EncodeDecodeUnsignedInt"), value);
+}
+
+TEST(JSONContainerTests, EncodeDecodeFloat) {
+    JSONContainer container;
+    
+    const double value = MAXFLOAT;
+    container.setFloatForKey(value, "EncodeDecodeFloat");
+    
+    ASSERT_EQ(container.floatForKey("EncodeDecodeFloat"), value);
 }
 
 TEST(JSONContainerTests, EncodeDecodeBool) {
@@ -26,8 +44,8 @@ TEST(JSONContainerTests, EncodeDecodeBool) {
     
     const bool a = true;
     const bool b = false;
-    container.encodeBoolForKey(a, "EncodeDecodeBool_A");
-    container.encodeBoolForKey(b, "EncodeDecodeBool_B");
+    container.setBoolForKey(a, "EncodeDecodeBool_A");
+    container.setBoolForKey(b, "EncodeDecodeBool_B");
     
     ASSERT_EQ(a, container.boolForKey("EncodeDecodeBool_A"));
     ASSERT_EQ(b, container.boolForKey("EncodeDecodeBool_B"));
@@ -52,12 +70,64 @@ std::string randomString(size_t length) {
 TEST(JSONContainerTests, EncodeDecodeString) {
     JSONContainer container;
     
-    auto str = randomString(0xFFFFF);
-    container.encodeStringForKey(str, "EncodeDecodeString");
+    auto str = randomString(0xFFF);
+    container.setStringForKey(str, "EncodeDecodeString");
     
     ASSERT_EQ(str, container.stringForKey("EncodeDecodeString"));
 }
 
-TEST(JSONContainerTests, Encod) {
+TEST(JSONContainerTests, EncodeDecodeEmbeddedContainer) {
+    JSONContainer container;
+    auto embeddedContainer = container.requestNestedContainer();
     
+    const int intValue = rand();
+    embeddedContainer->setIntForKey(intValue, "A");
+    
+    const unsigned int uIntValue = UINT_MAX;
+    embeddedContainer->setUnsignedIntForKey(uIntValue, "E");
+    
+    const double doubleValue = MAXFLOAT;
+    embeddedContainer->setFloatForKey(doubleValue, "F");
+    
+    const bool boolValue = rand() % 1;
+    embeddedContainer->setBoolForKey(boolValue, "B");
+    
+    auto stringValue = randomString(0xFFF);
+    embeddedContainer->setStringForKey(stringValue, "C");
+    
+    auto nestedContainer = embeddedContainer->requestNestedContainer();
+    
+    const int intValue2 = rand();
+    nestedContainer->setIntForKey(intValue2, "A");
+    
+    const unsigned int uIntValue2 = UINT_MAX;
+    nestedContainer->setUnsignedIntForKey(uIntValue2, "E");
+    
+    const double doubleValue2 = MAXFLOAT;
+    nestedContainer->setFloatForKey(doubleValue2, "F");
+    
+    const bool boolValue2 = rand() % 1;
+    nestedContainer->setBoolForKey(boolValue2, "B");
+    
+    auto stringValue2 = randomString(0xFFF);
+    nestedContainer->setStringForKey(stringValue2, "C");
+    
+    embeddedContainer->submitNestedContainerForKey(std::move(nestedContainer), "D");
+    container.submitNestedContainerForKey(std::move(embeddedContainer), "D");
+    
+    auto decodedEmbeddedContainer = container.containerForKey("D");
+    ASSERT_TRUE(decodedEmbeddedContainer != nullptr);
+    ASSERT_EQ(decodedEmbeddedContainer->intForKey("A"), intValue);
+    ASSERT_EQ(decodedEmbeddedContainer->unsignedIntForKey("E"), uIntValue);
+    ASSERT_EQ(decodedEmbeddedContainer->floatForKey("F"), doubleValue);
+    ASSERT_EQ(decodedEmbeddedContainer->boolForKey("B"), boolValue);
+    ASSERT_EQ(decodedEmbeddedContainer->stringForKey("C"), stringValue);
+    
+    auto decodedNestedContainer = decodedEmbeddedContainer->containerForKey("D");
+    ASSERT_TRUE(decodedNestedContainer != nullptr);
+    ASSERT_EQ(decodedNestedContainer->intForKey("A"), intValue2);
+    ASSERT_EQ(decodedNestedContainer->unsignedIntForKey("E"), uIntValue2);
+    ASSERT_EQ(decodedNestedContainer->floatForKey("F"), doubleValue2);
+    ASSERT_EQ(decodedNestedContainer->boolForKey("B"), boolValue2);
+    ASSERT_EQ(decodedNestedContainer->stringForKey("C"), stringValue2);
 }
