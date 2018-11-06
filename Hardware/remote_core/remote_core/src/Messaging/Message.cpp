debe89766fda889d20ecf428b0569ed1c6510a3d
David Moore <mooredev@me.com>
2018-11-05 21:43:29 -0500

    Implement Basis for Message

--- Hardware/remote_core/remote_core/src/Messaging/Message.cpp
+++ Hardware/remote_core/remote_core/src/Messaging/Message.cpp
@@ -7,18 +7,25 @@
 //
 
 #include "Message.hpp"
+#include <uuid/uuid.h>
 
 using namespace RemoteCore;
 
 Message::Message() {
+    // Generate the message ID.
+    uuid_t encodedUUID;
+    uuid_generate(encodedUUID);
     
+    char uuidString[64];
+    uuid_unparse(encodedUUID, uuidString);
+    
+    messageID = std::string(uuidString);
 }
 
-void Message::encodeWithCoder(Coder *aCoder) {
-    int i = 0;
-    aCoder->encodeIntForKey(i, "i");
+void Message::encodeWithCoder(Coder *aCoder) const {
+    aCoder->encodeStringForKey(messageID, "messageID");
 }
 
-void Message::decodeWithCoder(Coder *aCoder) {
-    auto demo = aCoder->decodeObjectForKey<Message>("Hello");
+void Message::decodeWithCoder(const Coder *aCoder) {
+    messageID = aCoder->decodeStringForKey("messageID");
 }
