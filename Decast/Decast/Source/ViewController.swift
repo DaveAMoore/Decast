351151337010d36fdebbc2b47f8526f4b709f4c8
David Moore <mooredev@me.com>
2018-11-18 16:52:44 -0500

    Add dummy implementation

--- Decast/Decast/Source/ViewController.swift
+++ Decast/Decast/Source/ViewController.swift
@@ -6,15 +6,31 @@
 //  Copyright © 2018 David Moore. All rights reserved.
 //
 
-import UIKit
+import SFKit
+import RemoteKit
+import AWSCore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RKSessionManagerDelegate {
 
+    let session = RKSession(device: RKDevice(serialNumber: "LE2RDNFR44"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        AWSDDLog.add(AWSDDTTYLogger(), with: .verbose)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        session.sessionManager.delegate = self
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        session.activate()
+    }
+    
+    func sessionManager(_ sessionManager: RKSessionManager, presentAuthenticationViewController viewController: UIViewController) {
+        present(viewController, animated: false, completion: nil)
+    }
 }
 
