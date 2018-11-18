155d342d2887299cbb963cca273dc3a22226d202
David Moore <mooredev@me.com>
2018-11-18 16:51:46 -0500

    Add RKError

---
+++ RemoteKit/RemoteKit/Model Types/RKError.swift
@@ -0,0 +1,19 @@
+//
+//  RKError.swift
+//  RemoteKit
+//
+//  Created by David Moore on 11/18/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+import Foundation
+
+/// RemoteKit error representation.
+public enum RKError: Int, Codable, Error {
+    case unknown                    = -1
+    case noSignalWhileTraining      = -2
+    case trainingAlreadyInSession   = -3
+    case invalidDirective           = -4
+    case invalidParameters          = -5
+    case noTrainingSession          = -6
+}
