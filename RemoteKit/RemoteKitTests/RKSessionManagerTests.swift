4e65a8e53435e11bbe66ed70865b8ebc5218c0ca
David Moore <mooredev@me.com>
2018-11-18 00:05:04 -0500

    Add RKSessionManagerTests

---
+++ RemoteKit/RemoteKitTests/RKSessionManagerTests.swift
@@ -0,0 +1,26 @@
+//
+//  RKSessionManagerTests.swift
+//  RemoteKitTests
+//
+//  Created by David Moore on 11/17/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+import XCTest
+import RFCore
+@testable import RemoteKit
+
+class RKSessionManagerTests: XCTestCase {
+
+    // MARK: - Lifecycle
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    // MARK: - Tests
+}
