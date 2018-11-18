9971143c7e93a6bc04b2cd975c5d1bfcd34beb6e
David Moore <mooredev@me.com>
2018-11-18 16:49:04 -0500

    Add Directives

--- remote_core/remote_core/include/TrainingSession.hpp
+++ remote_core/remote_core/include/TrainingSession.hpp
@@ -14,6 +14,19 @@
 #include "Remote.hpp"
 #include "Error.hpp"
 
+#define START_TRAINING_SESSION_DIRECTIVE "startTrainingSession"
+#define SUSPEND_TRAINING_SESSION_DIRECTIVE "suspendTrainingSession"
+#define CREATE_COMMAND_DIRECTIVE "createCommandWithLocalizedTitle"
+#define LEARN_COMMAND_DIRECTIVE "learnCommand"
+
+#define TRAINING_SESSION_DID_BEGIN_DIRECTIVE "trainingSessionDidBegin"
+#define TRAINING_SESSION_DID_FAIL_WITH_ERROR_DIRECTIVE "trainingSessionDidFailWithError"
+#define TRAINING_SESSION_WILL_LEARN_COMMAND_DIRECTIVE "trainingSessionWillLearnCommand"
+#define TRAINING_SESSION_DID_LEARN_COMMAND_DIRECTIVE "trainingSessionDidLearnCommand"
+#define TRAINING_SESSION_DID_REQUEST_INCLUSIVE_ARBITRARY_INPUT_DIRECTIVE "trainingSessionDidRequestInclusiveArbitraryInput"
+#define TRAINING_SESSION_DID_REQUEST_INPUT_FOR_COMMAND_DIRECTIVE "trainingSessionDidRequestInputForCommand"
+#define TRAINING_SESSION_DID_REQUEST_EXCLUSIVE_ARBITRARY_INPUT_DIRECTIVE "trainingSessionDidRequestExclusiveArbitraryInput"
+
 namespace RemoteCore {
     class HardwareController;
     class TrainingSessionDelegate;
@@ -31,6 +44,10 @@ namespace RemoteCore {
         
         std::string getSessionID(void) {
             return sessionID;
+        }
+        
+        Remote getAssociatedRemote(void) {
+            return associatedRemote;
         }
         
         std::weak_ptr<TrainingSessionDelegate> getDelegate(void) {
@@ -68,6 +85,7 @@ namespace RemoteCore {
         virtual void trainingSessionDidFailWithError(TrainingSession *session, Error error) {};
         
         // The training session is going to begin learning the command.
+        virtual void trainingSessionWillLearnCommand(TrainingSession *session, Command command) {};
         virtual void trainingSessionDidLearnCommand(TrainingSession *session, Command command) {};
         
         // Inclusive arbitrary input indicates all buttons should be pressed – in no specific order (i.e., arbitrary).
@@ -76,7 +94,7 @@ namespace RemoteCore {
         // Input should be provided for a single command, that is the one that is passed as a parameter.
         virtual void trainingSessionDidRequestInputForCommand(TrainingSession *session, Command command) {};
         
-        // Exclusive arbitrary input indicates that a single arbitrary command (i.e., button) should be pressed.
+        // Exclusive arbitrary input indicates that a single arbitrary command (i.e., button) should be pressed
         virtual void trainingSessionDidRequestExclusiveArbitraryInput(TrainingSession *session) {};
     };
 }
