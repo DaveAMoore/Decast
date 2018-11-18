1270de78d9d430c16baa598e41f89048a4e4c36d
David Moore <mooredev@me.com>
2018-11-18 00:04:36 -0500

    Add Constants

---
+++ RemoteKit/RemoteKit/Supporting Files/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RemoteKit
+//
+//  Created by David Moore on 11/17/18.
+//  Copyright © 2018 David Moore. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct RFCore {}
+}
