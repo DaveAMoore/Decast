c9761f6eb65a7b72faeb596b9fba5e545d066bfa
David Moore <mooredev@me.com>
2018-11-19 01:08:28 -0500

    Implement createCommandWithLocalizedTitle

--- remote_core/remote_core/src/Training/TrainingSession.cpp
+++ remote_core/remote_core/src/Training/TrainingSession.cpp
@@ -11,27 +11,56 @@
 #include "Coder.hpp"
 #include "JSONContainer.hpp"
 #include <exception>
+#include <iostream>
 
 using namespace RemoteCore;
 
 TrainingSession::TrainingSession(Remote associatedRemote) : associatedRemote(associatedRemote) {
     sessionID = UUID::GenerateUUIDString();
     
-    // availableCommandIDs =
+    // Read the command IDs in.
+    auto container = std::make_unique<JSONContainer>(std::ifstream("config/remote_core_command_ids.json"));
+    auto coder = std::make_unique<Coder>(std::move(container));
     
     // Sort a copied version of the available command IDs.
-    auto sortedCommandIDs = availableCommandIDs;
+    auto sortedCommandIDs = coder->decodeRootArray<std::string>();
     std::sort(sortedCommandIDs.begin(), sortedCommandIDs.end());
     
-    auto sortedCommands = associatedRemote.commands;
-    std::sort(sortedCommands.begin(), sortedCommands.end(), [](Command &lhs, Command &rhs) {
-        return lhs.getCommandID() > rhs.getCommandID();
-    });
+    std::vector<std::string> sortedRemoteCommandIDs;
+    for (auto &command : associatedRemote.commands) {
+        sortedRemoteCommandIDs.push_back(command.getCommandID());
+    }
+    std::sort(sortedRemoteCommandIDs.begin(), sortedRemoteCommandIDs.end());
+    
+    availableCommandIDs = std::vector<std::string>();
+    std::set_difference(sortedCommandIDs.begin(), sortedCommandIDs.end(),
+                        sortedRemoteCommandIDs.begin(), sortedRemoteCommandIDs.end(),
+                        std::inserter(availableCommandIDs, availableCommandIDs.end()));
+}
+
+void TrainingSession::start(void) {
+    /* ***************** Start the training session. ***************** */
+    
+    // Call the appropriate delegate method.
+    if (auto delegate = this->delegate.lock()) {
+        delegate->trainingSessionDidBegin(this);
+    }
+}
+
+void TrainingSession::suspend(void) {
+    /* ***************** Stop the training session. ***************** */
 }
 
 Command TrainingSession::createCommandWithLocalizedTitle(std::string localizedTitle) {
-    auto command = Command("[Command ID Here]", localizedTitle);
+    if (availableCommandIDs.empty()) {
+        throw std::logic_error("Expected 'availableCommandIDs' to be non-empty.");
+    }
+    
+    auto command = Command(localizedTitle, availableCommandIDs.back());
     associatedRemote.commands.push_back(command);
+    availableCommandIDs.pop_back();
+    
+    // TODO: Handle the case when all of the available command IDs are gone.
     
     return command;
 }
@@ -41,12 +70,10 @@ void TrainingSession::learnCommand(Command command) {
         throw std::logic_error("Expected 'currentCommand' to be empty.");
     }
     
-    /* if (command.getAssociatedRemote() != associatedRemote) {
-        throw std::logic_error("Expected 'command.associatedRemote' to be equal to 'associatedRemote'.");
-    } */
-    
     // Call the delegate.
     if (auto delegate = this->delegate.lock()) {
         delegate->trainingSessionWillLearnCommand(this, command);
     }
+    
+    /* ***************** Learn the command. ***************** */
 }
