4b1088909c341fdfb7090150e4e46eb3ca9a6d8f
David Moore <david@mooredev.ca>
2018-11-19 17:32:26 -0500

    Disable threading

--- remote_core/remote_core/src/Controllers/HardwareController.cpp
+++ remote_core/remote_core/src/Controllers/HardwareController.cpp
@@ -22,7 +22,7 @@ void HardwareController::sendCommandForRemoteWithCompletionHandler(Command comma
     /* ***************** Send the command. ***************** */
     
     // Create a lirc thread that handles the sending.
-    auto lircThread = std::thread([completionHandler]() {
+    // auto lircThread = std::thread([completionHandler]() {
 //        // initialize lirc socket and store file descriptor
 //        int fd = lirc_init("remote_core", 0);
 //        if (fd == -1) {
@@ -44,11 +44,12 @@ void HardwareController::sendCommandForRemoteWithCompletionHandler(Command comma
 //        // Deinitialize lirc socket
 //        lirc_deinit();
 //
-        completionHandler(Error::None);
-    });
+//    });
     
     // Detach the thread.
-    lircThread.detach();
+    // lircThread.detach();
+    
+    completionHandler(Error::None);
 }
 
 std::shared_ptr<TrainingSession> HardwareController::newTrainingSessionForRemote(Remote remote) {
